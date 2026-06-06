38caaf41d3c35a3bc21e45c25abdadcea241f8aa
ramitin1385-glitch <rami.tin1385@gmail.com>
2026-06-07 02:04:14 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+TOKEN = "توکن_جدید_ربات"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🎲 ربات منچ جدید آماده است!"
+    )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot Started...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
