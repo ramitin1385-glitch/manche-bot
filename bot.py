c769483f744f9a7e485826c7db91c252fb79962c
ramitin1385-glitch <rami.tin1385@gmail.com>
2026-06-07 02:05:09 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "توکن_جدید_ربات"
+TOKEN = "8945308523:AAHNP6225eb6jqy2u_kf00x9iFeS_cdSlEQ"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
